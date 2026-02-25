01f5f2e76432f82d4bdece5b89fd1f33a0eae70c
jannet18 <janetngei18@gmail.com>
2026-02-25 22:45:02 +0300

    cart configuration

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,19 +1,26 @@
 import "dotenv/config";
 import * as schema from "./schema/index.js";
-import { Client } from "pg";
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL missing");
 }
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL!,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+  max: 1,
 });
-client.connect();
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL!,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+// client.connect();
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(pool, { schema });
 export * from "./schema/index.js";
