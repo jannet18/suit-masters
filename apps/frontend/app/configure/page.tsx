fb461ff2c99f74a267be65653832e69b0029ff39
jannet18 <janetngei18@gmail.com>
2026-03-14 10:46:55 +0300

    fixes:build error

--- apps/frontend/app/configure/page.tsx
+++ apps/frontend/app/configure/page.tsx
@@ -39,3 +39,27 @@
 //   // This return statement won't be reached due to redirect
 //   return null;
 // }
+
+import { api } from "@/lib/api/api-client";
+import { notFound } from "next/navigation";
+import { BespokeConfigurator } from "../components/Configurator";
+interface PageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function ConfigureProductPage({ params }: PageProps) {
+  const { slug } = await params;
+
+  // Fetch product data to pass to configurator
+  const res = await api.getProductBySlug(slug);
+
+  if (!res || !res?.success || !res.product) {
+    return notFound();
+  }
+
+  return (
+    <div className="bg-[#0f0f0f] min-h-screen">
+      <BespokeConfigurator slug={slug} />;
+    </div>
+  );
+}
